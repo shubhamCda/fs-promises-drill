529187d8152749ed56619fcf05a3e0d05bd8d95f
Shubham <shubham@gmail.com>
2024-09-27 12:11:05 +0530

    Add convert_file_lowercase function

--- problem_02.cjs
+++ problem_02.cjs
@@ -16,23 +16,45 @@ function problem_02_process() {
     })
     .then(() =>{
         console.log("uppercase.txt content updataed..!");
-        // return store_filenames(filenames_path, uppercase_file_path);
+        return store_filenames(filenames_path, uppercase_file_path);
+        
+    }).then(() =>{
+        console.log("filenames.txt updated..!");
+        return file_reader(uppercase_file_path);
         
     })
-    .catch((err) => err);
+    .then((data) =>{
+        return convert_file_lowercase(data, lowercase_file_path);
+    })
+    .then(() =>{
+        console.log("lowercase.txt updated..!");
+        return store_filenames(filenames_path, lowercase_file_path);
+        
+    })
+    .then(() =>{
+        console.log("filenames.txt updated..!");
+        
+    })
+    .catch((err) => console.log(err)
+    );
 }
 
 function convert_file_uppercase(data, path) {
     
     const uppercase_content = data.toUpperCase();
     
-    return file_writer(uppercase_content, path);
+    return file_writer(path, uppercase_content);
+}
+
+function convert_file_lowercase(data, path)  {
+    const lowercase_content = data.toLowerCase();
+
+    return file_writer(path, lowercase_content);
 }
 
 
-
 function store_filenames(path, fileName) {
-    return file_writer(path, fileName);
+    return fs.appendFile(path, fileName + '\n');
 }
 
 function file_reader(path) {
