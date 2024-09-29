42526ab0e24f9347f022e8fa36008e830b40a7f9
Shubham <shubham@gmail.com>
2024-09-30 02:26:11 +0530

    Fix function parameters in problem_02.cjs

--- problem_02.cjs
+++ problem_02.cjs
@@ -4,27 +4,27 @@ const fs = require("fs").promises;
 
 
 // Convert the content to uppercase & write to a new file.
-function convert_file_uppercase(data, dirPath) {
+function convert_file_uppercase(data, filePath) {
 
     const uppercase_content = data.toUpperCase();
 
-    return file_writer(dirPath, uppercase_content);
+    return file_writer(filePath, uppercase_content);
 }
 
 // function to convert content of new file to lower case. Then split the contents into sentences.Then write it to a new file.
-function convert_file_lowercase(data, dirPath) {
+function convert_file_lowercase(data, filePath) {
     const lowercase_content = data.toLowerCase();
 
     const sentences = lowercase_content.match(/[^.!?]+[.!?]+/g) || lowercase_content.split("\n");
 
-    return file_writer(dirPath, sentences.join(" "));
+    return file_writer(filePath, sentences.join(" "));
 }
 
 
 // sort the content, write it out to a new file.
-function sorting_file(data, dirPath) {
+function sorting_file(data, filePath) {
     const sorted_content = data.split(" ").sort((a, b) => a.localeCompare(b)).join("\n");
-    return file_writer(dirPath, sorted_content);
+    return file_writer(filePath, sorted_content);
 }
 
 // delete all the new files that are mentioned in that list simultaneously.
@@ -42,19 +42,19 @@ function delete_files(data) {
 }
 
 // Store the name of the new file in filenames.txt
-function store_filenames(dirPath, fileName) {
-    return fs.appendFile(dirPath, fileName + '\n');
+function store_filenames(filePath, fileName) {
+    return fs.appendFile(filePath, fileName + '\n');
 }
 
 
 // function to read the given file 
-function file_reader(dirPath) {
-    return fs.readFile(dirPath, "utf-8");
+function file_reader(filePath) {
+    return fs.readFile(filePath, "utf-8");
 
 }
 
-function file_writer(dirPath, data) {
-    return fs.writeFile(dirPath, data);
+function file_writer(filePath, data) {
+    return fs.writeFile(filePath, data);
 
 }
 
