bdac047dc161b57cef75ab0a7102bc6dc39a7f5e
Shubham <shubham@gmail.com>
2024-09-30 02:26:57 +0530

    Add create_and_delete_files function in test_problem_01.cjs

--- test/test_problem_01.cjs
+++ test/test_problem_01.cjs
@@ -1,11 +1,26 @@
 const path = require("path");
 
-const { create_and_delete_files } = require("../problem_01.cjs")
+const { create_directory, json_file_generator,  delete_json_files } = require("../problem_01.cjs")
 
 const json_file = path.join(__dirname, "../json-files");
 
-function main() {
-    create_and_delete_files(json_file, 5);
+function create_and_delete_files(json_file, count) {
+    create_directory(json_file)
+        .then(() => {
+            console.log("Directory created...!");
+            return json_file_generator(json_file, count);
+
+        })
+        .then((fileUrls) => {
+            console.log("fileUrls fetch successfully...");
+            delete_json_files(fileUrls);
+
+        })
+        .catch((err) => {
+            console.error(err);
+
+        });
 }
 
-main();
+
+create_and_delete_files(json_file, 5);
