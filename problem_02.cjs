f05835d348e271bba52508583c3ef5dd1ada4056
Shubham <shubham@gmail.com>
2024-10-02 09:39:48 +0530

    Add problem_02_process function

--- problem_02.cjs
+++ problem_02.cjs
@@ -1,7 +1,63 @@
 const fs = require("fs").promises;
+const path = require("path");
 
 // drill using promises from fs library.
 
+const input_file_path = path.join(__dirname, "lipsum.txt");
+const uppercase_file_path = path.join(__dirname, "uppercase.txt");
+const lowercase_file_path = path.join(__dirname, "lowercase.txt");
+const sorted_file_path = path.join(__dirname, "sorted.txt");
+const filenames_path = path.join(__dirname, "filenames.txt");
+
+
+function problem_02_process() {
+    file_reader(input_file_path)
+        .then((data) => {
+            return convert_file_uppercase(data, uppercase_file_path);
+        })
+        .then(() => {
+            console.log("uppercase.txt content updataed..!");
+            return store_filenames(filenames_path, uppercase_file_path);
+
+        }).then(() => {
+            console.log("filenames.txt updated..!");
+            return file_reader(uppercase_file_path);
+
+        })
+        .then((data) => {
+            return convert_file_lowercase(data, lowercase_file_path);
+        })
+        .then(() => {
+            console.log("lowercase.txt updated..!");
+            return store_filenames(filenames_path, lowercase_file_path);
+
+        })
+        .then(() => {
+            console.log("filenames.txt updated..!");
+            return file_reader(lowercase_file_path);
+
+        })
+        .then((data) => {
+            return sorting_file(data, sorted_file_path);
+        })
+        .then(() => {
+            console.log("sorted.txt updated successfully..!");
+            return store_filenames(filenames_path, sorted_file_path);
+
+        })
+        .then(() => {
+            return file_reader(filenames_path);
+        })
+        .then((link) => {
+            return delete_files(link);
+        })
+        .then(() => {
+            console.log("Files deleted successfull..!");
+
+        })
+        .catch((err) => console.log(err)
+        );
+}
 
 // Convert the content to uppercase & write to a new file.
 function convert_file_uppercase(data, filePath) {
@@ -30,7 +86,7 @@ function sorting_file(data, filePath) {
 // delete all the new files that are mentioned in that list simultaneously.
 function delete_files(data) {
     const seperate_file = data.split("\n");
-    console.log(seperate_file);
+    // console.log(seperate_file);
 
     const removed_files = [];
     seperate_file.forEach(link => {
@@ -59,4 +115,5 @@ function file_writer(filePath, data) {
 }
 
 
-module.exports = { file_reader, store_filenames, delete_files, sorting_file, convert_file_lowercase, convert_file_uppercase };
+module.exports = { problem_02_process };
+
